b3c44a70acfd331b28665268a2018ae080e06ea0
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-28 08:56:21 +0530

    Print in shell

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/bash
 
 echo Hello World
+
+# Multi line printing
+# syntax : echo -e "Line1\nLine2"
+
+## -e option is needed for \n to work
+## "" -> Double quote / single quote is needed for \n to work
+## \n -> Print a new line
+
+echo -e "Hello\nWorld"
+
+# Printing with Colors
+# Color Codes
+# 31  RED
+# 32  GREEN
+# 33  YELLOW
+# 34  BLUE
+# 35  MAGENTA
+# 36  CYAN
+# 0   RESET
+
+# Syntax : echo -e "\e[COLmMESSAGE\e[0m"
+echo -e "\e[33mYello Text"
+echo Normal Text
