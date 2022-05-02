30f7f239418be1ed3fb1171d9068be20368705b3
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-02 08:25:36 +0530

    Update shell-scripting/

---
+++ 07-functions.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# If we assign a name to a set of data it is called as variable.
+# If we assign a name to a set of commands it is called as function.
+
+# Declare function
+# Syntax:
+    #FUNCTION_NAME() {
+    #  commands
+    #}
+
+sample() {
+  echo I am a Sample Function
+}
+
+## Main Program
+sample 
