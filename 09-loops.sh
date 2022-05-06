48486f96615aabde93b94bb132518b40c88d36b4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-06 08:41:57 +0530

    Update shell-scripting/

---
+++ 09-loops.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Two loops , for and while
+# But when to go with which loop
+
+# For loop , If our inputs are a list of values/strings then we can go with for loop
+
+for fruit in apple banana orange
+do
+  echo Fruit Name = $fruit
+done
